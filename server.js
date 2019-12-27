7a76a422c8f47d5dac5d158d127f30d439c2a2f7
freehere107 <freehere107@hotmail.com>
2019-12-27 18:42:24 +0800

    new type

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ function RecoverPersonalSigned(call, callback) {
 }
 
 async function SignAndSendTransaction(call, callback) {
-    await tx.checkReceipt(call.request.param).then(function (hash) {
+    await tx.checkReceipt(call.request.param,call.request.method).then(function (hash) {
         callback(null, {message: hash});
     });
 }
